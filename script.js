eddccefff912d1e5897836a6fe5f5f434399aa75
Ana Glória <anaparente196@gmail.com>
2024-05-21 15:48:48 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('ianimais')
+console.log(animais)
+
+const grid = document.getElementsByClassName('grid')
+console.log(grid)
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+const linksInternos = document.querySelector('[href^="#"]')
+console.log(linksInternos)
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg[1])
