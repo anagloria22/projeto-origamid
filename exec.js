e264c1b21224c85a04a09148c63d8c46e2149321
Ana Glória <147454263+anagloria22@users.noreply.github.com>
2024-05-25 17:43:45 -0300

    update

---
+++ exec.js
@@ -0,0 +1,14 @@
+const img = document.querySelectorAll('img')
+console.log(img)
+
+const imgAnimais = document.querySelectorAll('img[src^="imagens/imagem"]')
+console.log(imgAnimais)
+
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+console.log(linksInternos)
+
+const tituloDesc = document.querySelector('.animais-descricao h2')
+console.log(tituloDesc)
+
+const ultimoP = document.querySelectorAll('footer > p')
+console.log(ultimoP)
