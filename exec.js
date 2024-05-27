d493fba4d01a8f0c1aca8ca08702821e028f1b0b
Ana Glória <147454263+anagloria22@users.noreply.github.com>
2024-05-26 23:53:37 -0300

    Update exec.js

--- exec.js
+++ exec.js
@@ -24,3 +24,15 @@ const textParagrafos = Array.from(paragrafos)
 textParagrafos.forEach(function(item) {
     console.log(item.innerText)
 })
+
+//classList retorna uma lista com as classes do elemento e permite adicionar, remover ou verificar quais tem
+const menu = document.querySelector('.menu')
+console.log(menu.classList)
+
+//getAttribute ou setAttribute são métodos que retornam ou definem de acordo com o atributo selecionado
+const img = document.querySelector('img')
+img.getAttribute('src') //valor do src
+img.setAttribute('alt', 'Texto Alternativo') //muda o alt
+img.hasAttribute('id') // ture/false
+img.removeAttribute('alt') //remove o alt
+img.hasAttributes() // true/false se tem algum atributo
