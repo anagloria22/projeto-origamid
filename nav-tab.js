3e22a104d9b4ebdb7f87733befccfd449ab1be92
Ana Glória <147454263+anagloria22@users.noreply.github.com>
2024-06-23 16:07:19 -0300

    update

--- nav-tab.js
+++ nav-tab.js
@@ -3,16 +3,19 @@
 
 const tabMenu = document.querySelectorAll('.js-tabmenu li')
 const tabContent = document.querySelectorAll('.js-tabcontent section')
+tabContent[0].classList.add('ativo')
 
-function activeTab(index) {
-    tabContent.forEach((section) => {
-        section.classList.remove('ativo')
-    })
-    tabContent[index].classList.add('ativo')
-} //ess função adiciona a classe ativo ao elemento selecionado e remove da que já tiver antes
+if(tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo')
+        })
+        tabContent[index].classList.add('ativo')
+    } //ess função adiciona a classe ativo ao elemento selecionado e remove da que já tiver antes
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', function() {
-        activeTab(index)
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', function() {
+            activeTab(index)
+        })
     })
-})
+}
